2f1699efa259604cf64f8582fc9c0d9d35f3fb6d
mahdi <zitounimehdi.it@gmal.com>
2020-09-16 18:34:19 +0100

    code type_size

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int inttype;
+float floattype;
+double doubletype;
+char chartype;
+
+
+printf("size of int :%1d \n", sizeof(inttype));
+printf("size of float:%1d\n", sizeof(floattype));
+printf("size of double:%1d\n", sizeof(doubletype));
+printf("size of char:%1d\n", sizeof(chartype));
+return (0);
+
+}
