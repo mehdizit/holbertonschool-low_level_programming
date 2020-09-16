4f6434812339478399de924f31965690c79248db
mahdi <zitounimehdi.it@gmal.com>
2020-09-16 18:44:53 +0100

    fixed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,10 +12,10 @@ long longtype;
 char chartype;
 
 
-printf("size of int :%1d \n", sizeof(inttype));
-printf("size of float:%1d\n", sizeof(floattype));
-printf("size of double:%1d\n", sizeof(longtype));
-printf("size of char:%1d\n", sizeof(chartype));
+printf("size of int :%i \n", sizeof(inttype));
+printf("size of float:%i\n", sizeof(floattype));
+printf("size of double:%i\n", sizeof(longtype));
+printf("size of char:%i\n", sizeof(chartype));
 return (0);
 
 }
