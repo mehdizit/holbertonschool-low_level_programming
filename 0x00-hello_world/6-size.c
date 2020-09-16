9ea382e15dcc93e7a939a2745fd4605ed9af5167
mahdi <zitounimehdi.it@gmal.com>
2020-09-16 18:57:49 +0100

    fiiiiiixxxxxed

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,12 +10,14 @@ int inttype;
 float floattype;
 long longtype;
 char chartype;
+long int longinttype;
 
 
-printf("size of int :%i \n", sizeof(inttype));
-printf("size of float:%i\n", sizeof(floattype));
-printf("size of double:%i\n", sizeof(longtype));
-printf("size of char:%i\n", sizeof(chartype));
+printf("size of an int :%i \n", sizeof(inttype));
+printf("size of a float:%i\n", sizeof(floattype));
+printf("size of a long:%i\n", sizeof(longtype));
+printf("size of a char:%i\n", sizeof(chartype));
+printf("size of a long int:%i\n", sizeof(longinttype));
 return (0);
 
 }
