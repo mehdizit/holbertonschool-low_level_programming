6c78390d5f7289b9c8539986df3fb6ec1ef18eca
mahdi <zitounimehdi.it@gmal.com>
2020-09-18 00:37:46 +0100

    fix

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+* main - prints char number 0-9
+*
+* Return: Always 0
+*/
+
+int main(void)
+{
+	int i;
+	for (i = '0'; i <= '9'; i++)
+	{
+		putchar(i);
+	}
+	putchart('\n');
+	return (0);
+}
