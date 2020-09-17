244c4da46168afdbddb9657fbfbe147075b8eb6b
mahdi <zitounimehdi.it@gmal.com>
2020-09-17 23:08:59 +0100

    print lower case exept q e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main -  Prints all the alphabets in lowercase then in upercase.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	char c;
+	
+	c = 'a';
+
+	while (c < 'z')
+	{
+		if (c == e || c == q)
+		{
+			c++;
+		}
+		putchar(c);
+		c++;
+	}
+	return (0);
+	
+}
