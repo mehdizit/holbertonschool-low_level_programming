40dc6257e0777d96e97d82b211985f7bd4f58106
mahdi <zitounimehdi.it@gmal.com>
2020-09-17 20:36:49 +0100

    fiiiix

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -7,17 +7,11 @@
 
 int main(void)
 {
-char ch = 'a', 'A';
-while (('a' <= 'z') && ('A' <= 'Z'))
-{
-for (ch = 'a'; ch <= 'z'; ch++)
-{
-putchar(ch);
-}
-for (ch = 'A'; ch <= 'Z'; ch++)
-{
-putchar(ch);
-}
+char i;
+for (i = 'a'; i < = 'z'; i++)
+putchar(i);
+for (i = 'A'; i < = 'Z'; i++)
+putchar(i);
 putchar('\n');
 return (0);
 }
