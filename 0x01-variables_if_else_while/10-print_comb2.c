285ff801234b0f1f79896984b8337b6e20e42baf
mahdi <zitounimehdi.it@gmal.com>
2020-09-18 11:22:54 +0100

    finally

---
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n, m;
+
+	for (n = '0';  n = '9'; n++)
+		for (m = '0'; m = '9'; m++)
+			if (n != 0 && m != 0)
+			{
+				putchar(',');
+					putchar(' ');
+			}
+			else
+			{
+				putchar(n);
+				putchar(m);
+			}
+	putchar('\n');
+return (0);
+}
