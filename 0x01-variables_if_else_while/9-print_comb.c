813ad6478772f66923a96f663a3a771b4e5c9873
mahdi <zitounimehdi.it@gmal.com>
2020-09-18 10:28:00 +0100

    fixx

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,4 +1,4 @@
-include <stdio.h>
+#include <stdio.h>
 /**
  * main - Entry point
  *
@@ -10,6 +10,7 @@ include <stdio.h>
 int main(void)
 {
 	int i;
+
 	for (i = 48; i < 58 ; i++)
 	{
 		putchar(i);
