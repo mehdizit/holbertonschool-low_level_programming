b87c46223ac21ccc182c3d4fe1490b0959deb7bc
mahdi <zitounimehdi.it@gmal.com>
2020-09-19 20:46:03 +0100

    size of types

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,13 +10,13 @@ int inttype;
 float floattype;
 long longtype;
 char chartype;
-long int longinttype;
+long long longlongtype;
 
 
 printf("size of an int :%i \n", sizeof(inttype));
 printf("size of a float:%i\n", sizeof(floattype));
 printf("size of a long:%i\n", sizeof(longtype));
 printf("size of a char:%i\n", sizeof(chartype));
-printf("size of a long int:%i\n", sizeof(longinttype));
+printf("size of a long int:%i\n", sizeof(longlongtype));
 return (0);
 }
