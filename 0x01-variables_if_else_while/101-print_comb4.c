5ae268ece460c211adb77a6c671deada6e69ce89
mahdi <zitounimehdi.it@gmal.com>
2020-09-20 21:25:07 +0100

    test

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i, j, k;
+
+	for (i = 48; i <= 55; i++)
+	{
+		for (j = 48; j <= 56; j++)
+		{
+			for (k = 48; k <= 57; k++)
+			{
+				if (k = 57)
+				{
+					j = j+1;
+					k = j+1;
+					putchar(i);
+					putchar(j);
+					putchar(k);
+					
+				}
+			}
+		}
+	}
+	return (0);
+}
