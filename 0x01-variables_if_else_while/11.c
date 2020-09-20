64ef070a132fed5fb2de036023d16aa8ea12bf3a
mahdi <zitounimehdi.it@gmal.com>
2020-09-20 19:07:31 +0100

    other example

---
+++ 0x01-variables_if_else_while/11.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+
+
+int main(void)
+{
+	int i, j;
+
+	for (i =48; i <=56 ; i++)
+	
+	{	for (j = i+1; j <= 57; j++)
+		{
+			putchar(i);
+			putchar(j);
+			if ((i != 56) && (j != 57))
+			{
+				
+				
+			putchar(',');
+			putchar(' ');
+			}
+		    
+		        else
+			{
+				putchar('\n');
+			}
+	        	
+		}
+	}
+	
+	return (0);
+}
