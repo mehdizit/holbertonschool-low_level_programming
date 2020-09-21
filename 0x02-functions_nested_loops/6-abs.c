70833db641a5f9eec75b4b9a20bc1f33f3c3753f
mahdi <zitounimehdi.it@gmal.com>
2020-09-21 23:43:55 +0100

    fix

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * main - check the code for Holberton School students.
+ *
+ *_abs(int)-check for absolute value.
+ *
+ *_islower -check for loweralphabets.
+ *
+ * Return: Always 0.
+ */
+int _abs(int);
+{
+       
+	
+	if (n < 0)
+	{
+	
+		n =( n-(2*n));
+		return (n);
+	}
+	
+	else
+	{
+		return (n);   
+	}
+}
