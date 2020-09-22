27bcd0384a029333f31a2d7211c2aec1f484e8cb
mahdi <zitounimehdi.it@gmal.com>
2020-09-22 12:46:14 +0100

    fix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -3,8 +3,14 @@
 int print_last_digit(int n)
 {
 	int lastdigit, c;
+
 	lastdigit = n  % 10;
-	c ='0' + lastdigit;
-	_putchar(c);
+	if (lastdigit <  0)
+	{
+		lastdigit = -lastdigit;
+		c = '0' + lastdigit;
+		_putchar(c);
 		return (lastdigit);
+	}
+	return (0);
 }
