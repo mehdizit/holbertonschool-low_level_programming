3b18433363d14362b1fb11640f74f3da31e60680
mahdi <zitounimehdi.it@gmal.com>
2020-09-22 19:28:43 +0100

    print every munite of the day

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,6 +1,22 @@
 #include "holberton.h"
 
-
+/**
+ * jack_bauer - prints every minute of the day
+ *
+ *return :void.
+ */
 void jack_bauer(void)
 {
-	
+	for (int i = 0; i <= 23; i++)
+	{
+		for (int j = 0; j <= 59; j++)
+		{
+			_putchar('0' + i / 10);
+			_putchar('0' + i % 10);
+			_putchar(':');
+			_putchar('0' + j / 10);
+			_putchar('0' + j % 10);
+			_putchar('\n');
+		}
+	}
+}
