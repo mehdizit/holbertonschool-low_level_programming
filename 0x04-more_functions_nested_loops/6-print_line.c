4edf7466f37fd925a08ff3909db6320ba4427bd0
mahdi <zitounimehdi.it@gmal.com>
2020-09-24 21:57:32 +0100

    print straight_lines

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -1,5 +1,16 @@
 #include "holberton.h"
+/**
+ * print_line - prints an underscore n times
+ * @n: number of times printed
+ *
+ * Return: void
+ */
 
 void print_line(int n)
 {
-	
+	for (int i = 0; i <= n; i++)
+	{
+		_putchar('_');
+	}
+	_putchar('\n');
+}
