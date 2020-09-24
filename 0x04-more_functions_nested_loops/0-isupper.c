130d2a5fb95029f9d1e5c1bd0908b3b070884404
mahdi <zitounimehdi.it@gmal.com>
2020-09-24 14:48:32 +0100

    uppercase

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -1,16 +1,16 @@
 #include "holberton.h"
 /**
- * _isupper-check for upper case. 
- *@c:char
- * Return: Always 0.
- */
+*_isupper-check for upper case.
+*@c:integer
+* Return: Always 0 or 1.
+*/
 int _isupper(int c)
 {
 	while ((c >= 41) && (c <= 90))
-			{
-				return (1);
-				_putchar('\n');
-			}
+	{
+		return (1);
+		_putchar('\n');
+	}
 	return (0);
 	_putchar('\n');
 }
