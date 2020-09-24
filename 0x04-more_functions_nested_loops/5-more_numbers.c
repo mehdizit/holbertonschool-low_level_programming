35aa97b8e720779e26fd30ed8ad9983d58c6eafc
mahdi <zitounimehdi.it@gmal.com>
2020-09-25 00:54:24 +0100

    fixxxxxxxxxxxxxxxxx

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -6,9 +6,11 @@
  */
 void more_numbers(void)
 {
-	for (int i = 0; i <= 9; i++)
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
 	{
-		for (int j = 0; j <= 14; j++)
+		for (j = 0; j <= 14; j++)
 		{
 			if (j >= 10)
 			_putchar('0' + (j / 10));
