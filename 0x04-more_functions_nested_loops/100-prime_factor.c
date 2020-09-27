a4e0eeb5fdcdf887534bbf57d9a9f6cf24b8c336
mahdi <zitounimehdi.it@gmal.com>
2020-09-27 23:29:18 +0100

    largest_prime_factor

--- 0x04-more_functions_nested_loops/100-prime_factor.c
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -1,29 +1,27 @@
 
 #include<stdio.h>
- 
 /**
 *main-enter point_check for largestprime factor of n.
-*@i: integer
-*@div: integer
-*@n: unsigned long
+*
 *Return: Always 0.
 */
 int main(void)
 {
 	unsigned long n;
+
 	n = 612852475143;
 
-	int i =2 ,div = 2;
+	int i = 2, div = 2;
 
-	while(n != 1)
+	while (n != 1)
 	{
-		while(n % i == 0)
+		while (n % i == 0)
 		{
 			n = n / i;
-			printf("%d * ",i);
-			if(i > div)
+			printf("%d * ", i);
+			if (i > div)
 			{
-			        div = i;
+				div = i;
 			}
 		}
 		i++;
