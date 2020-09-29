4379e9f3a1824e3f516d2e202e463716bb1855c5
mahdi <zitounimehdi.it@gmal.com>
2020-09-29 23:52:23 +0100

    reverse string

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+
+/**
+*rev_string -reverse string
+*
+*@s: pointer to string
+*
+*/
+void rev_string(char *s)
+{
+	int i, j;
+	char temp;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j < (i / 2); j++)
+		{
+			temp = s[j];
+			s[j] = s[i - j - 1];
+			s[i - j - 1] = temp;
+		}
+	}
+}
