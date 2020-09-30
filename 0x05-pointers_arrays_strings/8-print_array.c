ceaf317cbc1d843f0b1cdd774894c36bdd03a799
mahdi <zitounimehdi.it@gmal.com>
2020-09-30 13:25:42 +0100

    update

---
+++ 0x05-pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+  *print_array - prints n elements of array
+  *@a: pointer to array
+  *@n: number of elements of array
+  *
+  */
+void print_array(int *a, int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != (n - 1))
+		{
+			printf(", ");
+		}
+	}
+		printf("\n");
+}
