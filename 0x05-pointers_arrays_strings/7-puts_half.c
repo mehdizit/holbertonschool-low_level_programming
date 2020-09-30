794b3987429a88cb31b3cbd3d5e06e0bb2ee23e8
mahdi <zitounimehdi.it@gmal.com>
2020-09-30 10:45:14 +0100

    half string

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+* puts_half : puts the second half of the string.
+*
+*@str: pointer to string
+*
+*/
+void puts_half(char *str)
+{
+	int i;
+	char *p = str;
+	int lenth = 0;
+	int half;
+
+	while (*p)
+	{
+		lenth++;
+		p++;
+	}
+	half = (lenth / 2);
+	for (i = half; i <= lenth; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
