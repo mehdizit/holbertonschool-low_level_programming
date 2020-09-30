8a58fb2bb5e0f9aa189945b4c80ec881872b544c
mahdi <zitounimehdi.it@gmal.com>
2020-09-30 22:58:03 +0100

    convert to integerwithout atoi function

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int _atoi(char *s)
+{
+	int i, res, sign, result;
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		if (s[i] < '9'|| s[i] > '0')
+		{
+			res = res * 10 + s[i] - '0';
+		}
+		else if (s[i] == '-')
+		{
+			sign = sign * 10 + s[i] - '0';
+		}
+		result = sign * res;
+	}
+	return (result);
+}
