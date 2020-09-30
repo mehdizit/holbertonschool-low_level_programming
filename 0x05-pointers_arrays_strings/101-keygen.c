5ec8d4de98ada678ac768561c897f9bd9aa89ab4
mahdi <zitounimehdi.it@gmal.com>
2020-09-30 23:58:12 +0100

    start

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -1,6 +1,21 @@
-#include "holberton.h"
 #include <stdio.h>
-
+#include <stdlib.h>
+/**
+ *main -enter point to check the code
+ *@i:integer
+ *
+ *Always return 0.
+ */
 int main(void)
 {
+	int i;
+
+	for(i = 0; i < 12; i++)
+	{
+		pass[i] = 33 + rand() % 94;
+	}
+	pass[i] = '\0';
+	printf("%s\n",pass);
+	return (0);
+}
 	
