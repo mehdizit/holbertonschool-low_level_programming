4735ef25c0ff4cffce2a1a0c337940df19930036
mahdi <zitounimehdi.it@gmal.com>
2020-10-05 11:33:04 +0100

    return first occurence of char c

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ * _strchr - locates char in string
+ * @s: pointer to string
+ * @c: char to locate
+ *
+ * Return: pointer to first occurence of c in s
+ */
+char *_strchr(char *s, char c)
+{
+	while (*s)
+	{
+     
+		if (*s == c)
+		{
+			return (s);
+		}
+		s++;
+	}
+	return (0);
+}
