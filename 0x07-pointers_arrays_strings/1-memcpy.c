6c08d742826fbdb22e6393c6064f41e1e35ac5b8
mahdi <zitounimehdi.it@gmal.com>
2020-10-05 10:43:32 +0100

    copy memory area

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,23 @@
+#include "holberton.h"
+/**
+ * *_memcpy -  copy memory from src to dest
+ *
+ *@dest:char
+ *
+ *@src:char
+ *
+ *@n:unsigned int
+ *
+ * Return: char.
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	while (dest && n > 0)
+	{
+		*dest = *src;
+		dest++;
+		src++;
+		n--;
+	}
+	return (dest);
+}
