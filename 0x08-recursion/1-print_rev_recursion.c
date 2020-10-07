afefac2b8081ad599ced19fb03abf70e05a4f9d2
mahdi <zitounimehdi.it@gmal.com>
2020-10-07 11:04:11 +0100

    print string in reverse by recursion

---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ *_print_rev_recursion -  a function that prints string in reverse
+ *
+ *@s : string to print in reverse
+ *
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s)
+	{
+		_print_rev_recursion(s + 1);
+		_putchar(*s);
+	}
+	else
+	{
+		_putchar('\n');
+	}
+}
