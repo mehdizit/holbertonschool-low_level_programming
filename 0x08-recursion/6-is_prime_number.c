c2813004dbde12f24884a6ded4855ad1651407e4
mahdi <zitounimehdi.it@gmal.com>
2020-10-07 21:23:26 +0100

    finaly

--- 0x08-recursion/6-is_prime_number.c
+++ 0x08-recursion/6-is_prime_number.c
@@ -1,24 +1,37 @@
 #include "holberton.h"
-
+/**
+ * check_prime - Checks if a number is divisible.
+ * @n1: The number to be checked.
+ * @n2: The divisor.
+ *
+ * Return: prime number
+ *
+ */
 int check_prime(int n1, int n2)
 {
-	
 	if (n1 % n2 == 0)
 	{
 		return (0);
 	}
 	else if (n2 == n1 / 2)
 	{
-		return(1);
+		return (1);
 	}
 	return (check_prime(n1, n2 + 1));
-	
 }
+/**
+ * is_prime_number - Checks if a number is prime.
+ * @n: number to be checked.
+ *
+ * Return:prime number or not.
+ *
+ */
 
 
 int is_prime_number(int n)
 {
-	int n2 = 2; 
+	int n2 = 2;
+
 	if (n <= 1)
 	{
 		return (0);
