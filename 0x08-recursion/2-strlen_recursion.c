453f5f4891c3c94f6957ae7838e0ddce743ebc1c
mahdi <zitounimehdi.it@gmal.com>
2020-10-07 12:41:33 +0100

    lenght of string

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+/**
+ *_strlen_recursion - Return length of string
+ *
+ *@s : string
+ *
+ *Return: length of a string
+ */
+int _strlen_recursion(char *s)
+{
+	int length = 0;
+
+	if (*s != '\0')
+	{
+		length++;
+	length = length +	_strlen_recursion(s + 1);
+	}
+
+	return (length);
+}
+
