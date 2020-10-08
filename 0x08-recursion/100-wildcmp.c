a9b6bbb0cbfd530b99bf37a8c6f31a75677c88f9
mahdi <zitounimehdi.it@gmal.com>
2020-10-08 23:20:49 +0100

    this is test

--- 0x08-recursion/100-wildcmp.c
+++ 0x08-recursion/100-wildcmp.c
@@ -1,25 +1,47 @@
+/*
+ * 100-wildcmp.c
+ * Auth: Mehdi Zitouni
+ */
+
+
 #include "holberton.h"
 
+/**
+ * _movewild - move index to jump wild character.
+ *
+ *@s2: string
+ *@star: char
+ *
+ *Return: zero.
+ */
 int _movewild(char *s2, char star)
 {
 
 	star = '*';
-	if(*s2)
+	if (*s2)
 	{
 		if (*s2 == star)
 		{
 			return (_movewild(s2 + 1, star));
 		}
-	}return (0);
+	}
+	return (0);
 }
 
-
+/**
+ *wildcmp - function to compare two st wildcmpring s1 and s2.
+ *@s1: string to compare with s2.
+ *@s2: string to compare with s1.
+ *
+ *Return: zero.
+ */
 int wildcmp(char *s1, char *s2)
 {
 	char star = '*';
+
 	if (*s1 && *s2)
 	{
-		if (*(s1 + 1) == *(s2 +1))
+		if (*(s1 + 1) == *(s2 + 1))
 		{
 			return (1);
 		}
@@ -28,22 +50,6 @@ int wildcmp(char *s1, char *s2)
 			return (_movewild(s2 + 1, star));
 		}
 
-	}return (0);	
-}
-
-/**
- *
-     int _checks2star(char *s2, char *s3)
-     {
-	if (*s2 && *s3)
-	{
-		s3 = s2 + 1;
-		if (*s3 == star)
-		{
-			return (_checks2star(s2, s3 + 1));
-		}
 	}
 	return (0);
-      }
-
-*/
+}
