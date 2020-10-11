41f4ce2dfeaf4900e056b682672c81ac2b6d8eca
mahdi <zitounimehdi.it@gmal.com>
2020-10-11 16:51:07 +0100

    print name programme

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -8,9 +8,8 @@
  *Return: zero.
  *
  */
-int main (int __attribute__((unused)) argc, char *argv[])
-	
-{	
-	printf ("%s\n", argv[0]);
+int main(int __attribute__((unused)) argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
 	return (0);
 }
