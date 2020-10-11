dd53c6dd04a21cd891c3466eca50295718142010
mahdi <zitounimehdi.it@gmal.com>
2020-10-11 16:39:02 +0100

    print name of programme

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,9 @@
+#include "holberton.h"
+#include <stdio.h>
+
+int main (int __attribute__((unused)) argc, char *argv[])
+	
+{	
+	printf ("the name of this programme is %s\n", argv[0]);
+	return (0);
+}
