522beb0d529d0bccbc01f59a8fb60bd8f54ed23a
mahdi <zitounimehdi.it@gmal.com>
2020-10-11 16:44:02 +0100

    print name programme

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,6 +1,13 @@
 #include "holberton.h"
 #include <stdio.h>
-
+/**
+ *main - Prints the program name followed by a new line.
+ * @argc: The number of arguments
+ *
+ * @argv: An array of pointers to the arguments
+ *Return: zero.
+ *
+ */
 int main (int __attribute__((unused)) argc, char *argv[])
 	
 {	
