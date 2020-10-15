f0d4776f3426729adff4530a96c81ac53e7941e9
mahdi <zitounimehdi.it@gmal.com>
2020-10-15 17:40:35 +0100

    caaaaallllloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -9,22 +9,25 @@
  */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
+	void *array;
+	char *array1;
+	unsigned int i;
+
 	if (nmemb == 0  || size == 0)
 	{
 		return (NULL);
 	}
-	char *array;
-	unsigned int i;
+
 
 	array = malloc(nmemb * size);
 	if (array == NULL)
 	{
 		return (NULL);
 	}
-	for (i = 0; i < (nmemb * size); i++)
+	array1 = array;
+	for (i = 0; i < nmemb * size; i++)
 	{
-		array[i] = '\0';
+		array1[i] = '\0';
 	}
-	
 	return (array);
 }
