282f356d9e177b12380ddf9556b063c26f9c9fab
mahdi <zitounimehdi.it@gmal.com>
2020-10-15 15:11:44 +0100

    update

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -15,7 +15,10 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	char *strc;
 
 	len1 = len2 = 0;
-
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 	for (i = 0; s1[i]; i++)
 	{
 		len1++;
