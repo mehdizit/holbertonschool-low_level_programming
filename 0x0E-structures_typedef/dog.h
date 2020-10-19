a30beb18968036b24ebfad5392bbdb9d7e41bd02
mahdi <zitounimehdi.it@gmal.com>
2020-10-19 23:04:57 +0100

    dog struct

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,15 @@
-struct dog {
+#ifndef DOG_H
+#define DOG_H
+/**
+ * struct dog - all your doggy needs in one structure
+ * @name: name of dog
+ * @age: age of dog
+ * @owner: owner of dog
+ */
+struct dog
+{
 	char *name;
 	float age;
-	char * owner;
-           } dog;
+	char *owner;
+};
+#endif
