c9d94045f39696d8b29ba02fa1b9fb0d8972ac0d
mehdizit <zitounimehdi.it@gmail.com>
2020-10-22 20:25:32 +0100

    function add, mul, sub, div and modulo

--- 0x0F-function_pointers/3-op_functions.c
+++ 0x0F-function_pointers/3-op_functions.c
@@ -1,42 +1,56 @@
 #include "3-calc.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *op_add - function to add two integer
+ *@a: integer
+ *@b: integer
+ *Return: a+b.
+ */
 int op_add(int a, int b);
 {
-	
-
-
-
-
-
-
-
-
-
+	return (a + b);
+}
+/**
+ *op_sub - function to sub b from a
+ *@a: integer
+ *@b: integer
+ *Return: a-b.
+ */
 int op_sub(int a, int b);
 {
-
-
-
-
-	
+	return (a - b);
+}
+/**
+ *op_mul - function to multiply a and b
+ *@a: integer
+ *@b: integer
+ *Return: a*b.
+ */	
 int op_mul(int a, int b);
 {
-
-
-
-
-
-	
-
-
+	return (a * b);
+}
+/**
+ *op_div - function to b divid a and b
+ *@a: integer
+ *@b: integer
+ *Return: a/b.
+ */
 int op_div(int a, int b);
 {
-
-
-
-	
+	return (a / b);
+}
+/**
+ *op_ - function modulo a, b.
+ *@a: integer
+ *@b: integer
+ *Return: a % b.
+ */	
 int op_mod(int a, int b);
 {
+	return (a % b);
+}
 
 
 	
