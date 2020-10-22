cf32f0723d7553cd69346c460e215add726fac84
mehdizit <zitounimehdi.it@gmail.com>
2020-10-22 21:24:39 +0100

    update

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -1,5 +1,27 @@
 #include "3-calc.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+* get_op_func - that is function
+* @s: char pointer s
+*
+* Return: integer.
+*/
 int (*get_op_func(char *s))(int, int)
 {
-	
+	op_t ops[] = {
+		{"+", op_add},
+		{"-", op_sub},
+		{"*", op_mul},
+		{"/", op_div},
+		{"%", op_mod},
+		{NULL, NULL}
+	};
+	int i;
+
+	i = 0;
+	while (ops[i].f != NULL && strcmp(ops[i].op, s) != 0)
+		i++;
+	return (ops[i].f);
+}
