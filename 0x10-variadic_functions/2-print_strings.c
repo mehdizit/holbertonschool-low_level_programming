8ef832b7554f76b0fcc934cfa6d5ea271a8d013c
mehdizit <zitounimehdi.it@gmail.com>
2020-10-24 00:03:51 +0100

    fourth

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,16 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+#include <stdarg.h>
+
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	va_list ch;
+	unsigned int i;
+
+	va_start (ch, n);
+	for (i = 0; i < n; i++)
+	{
+		printf ("%s", va_arg (ch, char *));
+		printf ("%s", separator);
+	}
+}
