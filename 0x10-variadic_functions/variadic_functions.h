25830934128ceaa674b39bb2815aa12a048c9592
mehdizit <zitounimehdi.it@gmail.com>
2020-10-25 16:45:42 +0100

    update

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -1,10 +1,17 @@
 #ifndef _VARIADIC_FUNCTIONS_H
 #define _VARIADIC_FUNCTIONS_H
+#include <stdarg.h>
 
 int _putchar(char c);
 int sum_them_all(const unsigned int n, ...);
 void print_numbers(const char *separator, const unsigned int n, ...);
 void print_strings(const char *separator, const unsigned int n, ...);
 void print_all(const char * const format, ...);
+typedef struct print
+{
+        char *t;
+	void (*f)(va_list);
+} print_t;
+
 
 #endif
