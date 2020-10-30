8aa40df5ff8602fcbd96844a84bedc733acca0c8
mehdizit <zitounimehdi.it@gmail.com>
2020-10-30 21:24:51 +0100

    update

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -5,18 +5,24 @@
 
 list_t *add_node_end(list_t **head, const char *str)
 {
+	int i, len;
 	list_t *ptr;
 	list_t *temp;
 	ptr = *head;
 	temp = malloc(sizeof(list_t));
+	len = 0;
+	for (i = 0; str[len]; i++)
+		len++;
 	temp->str = strdup(str);
+	temp->len = len;
 	temp->next = NULL;
 	while (ptr->next != NULL)
 	{
+		ptr = *head;
 		ptr = ptr->next;
 	}
 	ptr->next = temp;
-	return (temp);
+	return (*head);
 }
 
 	
