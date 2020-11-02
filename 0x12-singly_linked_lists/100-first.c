e2362a970e421df1a0ecae3a2ce6ab201db06134
mehdizit <zitounimehdi.it@gmail.com>
2020-11-02 01:50:42 +0100

    pragma

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
-void init();
+/**
+ * init - Prints a string before the main function is executed.
+ *
+ */
+void init(void);
 void __attribute__ ((constructor)) init()
 {
 	printf("You're beat! and yet, you must allow,\n");
