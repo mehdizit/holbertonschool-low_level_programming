0cda212a8a4a14c6a8f2eac9c375099e4dbf7ee7
mehdizit <zitounimehdi.it@gmail.com>
2020-11-03 23:43:09 +0100

    insert at given position

---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,28 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *insert_nodeint_at_index - insert node at given index idx.
+ *@head: head of linked lists
+ *@idx: the position of node that we will insert new_node.
+ *@n: new data of node to be inserted
+ *Return: new linked lists.
+ */
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	unsigned int i;
+	listint_t *new_node, *temp;
+
+	temp = *head;
+	new_node = malloc(sizeof(listint_t));
+	new_node->n = n;
+	while (i < idx)
+	{
+		temp = temp->next;
+		i++;
+	}
+	new_node->next = temp->next;
+	temp->next = new_node;
+	return (new_node);
+}
