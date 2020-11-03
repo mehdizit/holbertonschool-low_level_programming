9d3ebd4150d0b9da4aa68743f6e37e5e7640568b
mehdizit <zitounimehdi.it@gmail.com>
2020-11-03 14:34:35 +0100

    start

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,12 +1,14 @@
 #ifndef _LISTS_H
 #define _LISTS_H
-
-int _putchar(char c);
-size_t print_listint(const listint_t *h);
-
+#include <stdlib.h>
 typedef struct listint_s
 {
     int n;
     struct listint_s *next;
 } listint_t;
+
+int _putchar(char c);
+size_t print_listint(const listint_t *h);
+
+
 #endif
