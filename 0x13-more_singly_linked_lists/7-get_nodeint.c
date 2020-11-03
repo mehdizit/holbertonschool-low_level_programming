44de4dbd67f0b8d6820a2b25f6a20e7d8f1b4ef9
mehdizit <zitounimehdi.it@gmail.com>
2020-11-03 22:13:18 +0100

    return linked lists at index node

---
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *get_nodeint_at_index - get the linked lists at index node.
+ *@head: head of linked lists
+ *@index: index of node
+ *Return: linked lists
+ */
+listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+
+	while (i < index)
+	{
+		head = head->next;
+		i++;
+	}
+	return (head);
+}
