ce1598296fd475f506f28bd354b08194e99b5ec3
mehdizit <zitounimehdi.it@gmail.com>
2020-11-03 21:50:13 +0100

    delete first node

---
+++ 0x13-more_singly_linked_lists/6-pop_listint.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *pop_listint - delete first node
+ *@head: head of linked list
+ *Return: new linked lists after deletion first node
+ */
+int pop_listint(listint_t **head)
+{
+	if (*head == NULL)
+		return (0);
+	else
+	{
+		listint_t *temp;
+
+		temp = *head;
+		*head = (*head)->next;
+		free(temp);
+	}
+	return ((*head)->n);
+}
