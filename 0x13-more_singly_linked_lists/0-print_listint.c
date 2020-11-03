790dac34f0904c9fe7090f5908abf3da695a9f4a
mehdizit <zitounimehdi.it@gmail.com>
2020-11-03 14:42:38 +0100

    display number and content of node linked lists

---
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,15 @@
+#include "lists.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+size_t print_listint(const listint_t *h)
+{
+	int i = 0;
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+		i++;
+	}
+	return (i);
+}
