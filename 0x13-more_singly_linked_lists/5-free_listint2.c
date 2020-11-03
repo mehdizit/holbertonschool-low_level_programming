f3aa9f0240e95e6d505df982f03853a452efd79c
mehdizit <zitounimehdi.it@gmail.com>
2020-11-03 20:41:39 +0100

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -2,6 +2,19 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *free_listint2 - to free linked lists
+ *@head: head of linked lists
+ */
+
 void free_listint2(listint_t **head)
 {
-	
+	listint_t *ptr;
+
+	while (*head != NULL)
+	{
+		ptr = (*head)->next;
+		free(*head);
+		*head = ptr;
+	}
+}
