4e21f778bd5029bcad8badad16eb9a37d9618d31
mehdizit <zitounimehdi.it@gmail.com>
2020-11-04 22:54:37 +0100

    update

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -16,11 +16,14 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *previous = *head;
 
 	i = 0;
-	if (i == 1)
+	if (*head == NULL)
+		return (-1);
+	else if (index == 0)
 	{
 		*head = current->next;
 		free(current);
 		current = NULL;
+		return (1);
 	}
 	else
 	{
@@ -31,6 +34,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 			current = current->next;
 			i++;
 		}
+		if (i < index)
+			return (-1);
 		previous->next = current->next;
 		free(current);
 		current = NULL;
