316866d2371e4c85f69d0a5da8c9cb113c2e3ff2
mehdizit <zitounimehdi.it@gmail.com>
2020-11-04 17:21:48 +0100

    update

--- 0x13-more_singly_linked_lists/8-sum_listint.c
+++ 0x13-more_singly_linked_lists/8-sum_listint.c
@@ -2,9 +2,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ *sum_listint - fuction to return sum of all data node of linked lists
+ *@head: pointer to the first node of linked lists
+ *Return: sum of data in integger
+ */
 int sum_listint(listint_t *head)
 {
 	int sum = 0;
+
 	while (head)
 	{
 		sum = sum + head->n;
