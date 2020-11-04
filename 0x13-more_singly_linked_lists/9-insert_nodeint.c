a520c6db13308c2b2c26f51a844c8d80eb36b8d0
mehdizit <zitounimehdi.it@gmail.com>
2020-11-04 23:08:47 +0100

    update
    xit
    exit

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -19,6 +19,18 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	if (!head || !new_node)
 		return (NULL);
 	new_node->n = n;
+	if (*head == NULL)
+	{
+		*head = new_node;
+		new_node->next = NULL;
+		return (new_node);
+	}
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		*head = new_node;
+		return (new_node);
+	}
 	while (i < idx)
 	{
 		temp = temp->next;
