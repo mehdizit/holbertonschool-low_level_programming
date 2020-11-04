6d17455b7a7b4d27a0e9f6106fd823cc919f737e
mehdizit <zitounimehdi.it@gmail.com>
2020-11-04 17:15:50 +0100

    update

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -12,8 +12,11 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 {
 	unsigned int i;
 
+	i = 0;
 	while (i < index)
 	{
+		if (head == NULL)
+			return (NULL);
 		head = head->next;
 		i++;
 	}
