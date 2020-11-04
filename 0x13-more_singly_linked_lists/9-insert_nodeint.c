329758227d38b012018a4739c617a9852114a2a5
mehdizit <zitounimehdi.it@gmail.com>
2020-11-04 17:26:04 +0100

    update

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -16,6 +16,8 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 
 	temp = *head;
 	new_node = malloc(sizeof(listint_t));
+	if (!head || !new_node)
+		return (NULL);
 	new_node->n = n;
 	while (i < idx)
 	{
