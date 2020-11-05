5b16bd32399f74ebdbd9c7aa4b22a09b310788a8
mehdizit <zitounimehdi.it@gmail.com>
2020-11-05 21:19:33 +0100

    test

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,4 +2,19 @@
 
 unsigned int binary_to_uint(const char *b)
 {
+	int dec = 0, w = 1;
+	int rem, a;
+	int i = 0;
+
+	while (b[i] != 0)
+	{
+		rem = (b[i] - '0') % 10;
+		dec = dec + rem * w;
+		a = (b[i] - '0');
+		a = a / 10;
+		w = w * 2;
+		i++;
+	}
+	return (a);
+}
 	
