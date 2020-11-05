3e17157c093519889e925f0ede1a2b25a314abf1
mehdizit <zitounimehdi.it@gmail.com>
2020-11-05 21:51:28 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -2,16 +2,15 @@
 
 unsigned int binary_to_uint(const char *b)
 {
-	int dec = 0, w = 1;
+	unsigned int dec = 0;
 	int i = 0;
 
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-		
-		dec = dec + ((b[i] - '0') * w);
-		w = w * 2;
+	       
+		dec = dec * 2  + (b[i] - '0');
 		i++;
 	}
 	return (dec);
