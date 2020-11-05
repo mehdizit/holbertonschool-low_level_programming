6d9d15407640b99ff0728b4c5256993b9439f1b9
mehdizit <zitounimehdi.it@gmail.com>
2020-11-05 21:57:40 +0100

    update

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,18 +1,23 @@
 #include "holberton.h"
-
+/**
+ *binary_to_uint - convert binary to decimal
+ *@b: char to convert to int
+ *Return: integger
+ */
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int dec = 0;
 	int i = 0;
 
+	if (b == NULL)
+		return (0);
+
 	while (b[i] != '\0')
 	{
 		if (b[i] != '0' && b[i] != '1')
 			return (0);
-	       
 		dec = dec * 2  + (b[i] - '0');
 		i++;
 	}
 	return (dec);
 }
-	
