00464126fc7e96a3fa0ef8215305d597ab5ef942
mehdizit <zitounimehdi.it@gmail.com>
2020-11-06 21:17:54 +0100

    flip

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -26,10 +26,8 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
 	unsigned long int n_xor_b;
 	int a;
+
 	n_xor_b = n ^ m;
 	a = count_set_bit(n_xor_b);
 	return (a);
 }
-	
-	
-	
