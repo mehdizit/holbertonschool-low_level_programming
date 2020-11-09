238666ab2e713bf870197487c3084ac1d078926a
mehdizit <zitounimehdi.it@gmail.com>
2020-11-09 15:47:09 +0100

    update

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -11,12 +11,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	
 	fd = open(filename, O_RDONLY);	
 		
-	
-	if (fd == -1)
-	{
-		free(buf);
-		return (0);
-	}
+        
 	buf = malloc(sizeof(char) * letters);
 		if (buf == NULL)
 		{
