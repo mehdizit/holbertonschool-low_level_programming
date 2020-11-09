4a3c29f4b2f827330e246f0fc93dd7291b865dfa
mehdizit <zitounimehdi.it@gmail.com>
2020-11-09 15:42:37 +0100

    header

--- 0x15-file_io/holberton.h
+++ 0x15-file_io/holberton.h
@@ -2,6 +2,10 @@
 #define HOLBERTON_H
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
 int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
