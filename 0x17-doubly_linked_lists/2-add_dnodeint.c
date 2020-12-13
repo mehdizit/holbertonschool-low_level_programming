405f79ad51256ab836e895b1cb988908220686be
mehdizit <zitounimehdi.it@gmail.com>
2020-12-14 00:33:14 +0100

    add node

---
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
+	dlistint_t *node;
+
+	node = malloc(sizeof(dlistint_t));
+	if (node = NULL)
+		return (NULL);
+	node->n = n;
+	node->prev = NULL;
+	node->next = *head;
+	if (*head != NULL)
+		(*head)->prev = node;
+	*head = node;
+	return (node);
+}
