f41d12677d95b2d2bc07a29bd7ee4042b4ed6ee5
mehdizit <zitounimehdi.it@gmail.com>
2021-04-20 17:34:36 +0100

    up

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -7,5 +7,5 @@ int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
-void print_array(int *array, size_t position, size_t size);
+void print_array(int *array, size_t pos, size_t size);
 #endif
