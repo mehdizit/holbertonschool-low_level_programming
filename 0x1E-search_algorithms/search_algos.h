089d79f6fc399a3a84343204fcb03f54a3caa3a8
mehdizit <zitounimehdi.it@gmail.com>
2021-04-20 17:13:36 +0100

    up

---
+++ 0x1E-search_algorithms/search_algos.h
@@ -0,0 +1,11 @@
+#ifndef _SEARCH
+#define _SEARCH
+#include <stdio.h>
+#include <math.h>
+
+int linear_search(int *array, size_t size, int value);
+int binary_search(int *array, size_t size, int value);
+int jump_search(int *array, size_t size, int value);
+int interpolation_search(int *array, size_t size, int value);
+void print_array(int *array, size_t position, size_t size);
+#endif
